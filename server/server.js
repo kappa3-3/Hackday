4308f166be5cb8045428411051fd328a3826a28c
Kasia Janiszewska <katarzyna.janiszewska@gmail.com>
2020-07-30 09:35:09 +0200

    server basic set up

--- server/server.js
+++ server/server.js
@@ -1,1 +1,19 @@
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares);
+server.use(jsonServer.bodyParser);
+server.use('/playedgames', (req, res, next) => {
+  if (req.method === 'POST') {
+    res
+      .send(req.body);
+  }
+  next();
+});
+server.use(router);
+server.listen(8080, () => {
+  console.log('JSON Server running on port 8080');
+});
 
